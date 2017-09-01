0d1d8b790b565e4ed77ddfc35c4e69e4a8a00f85
ro-carlos <30359354+ro-carlos@users.noreply.github.com>
2017-08-31 22:07:32 -0500

    Agregue saltos de linea

--- primero.m
+++ primero.m
@@ -8,23 +8,25 @@ inferior = 0;
 superior = 0;
 distancia2 = 10;
 
+
+
+
 while acum < tolerancia
     p  = feval(f,acum);
     gp  = feval(g,acum);
     distancia = abs(gp-p);
-    
 
-       
+
+
     if(distancia < distancia2)%evaluaos los limites
         distancia2 = distancia;
          inferior = acum;
          superior = acum+0.1;
     end
     acum = acum+0.1;
-     
+
     %fprintf('p: %d  gp: %d  distancia: %d\n',p,gp,distancia);
     %fprintf('inferior: %d  superior: %d\n',inferior, superior);
 end
 
 fprintf('inferior: %d  superior: %d\n',inferior, superior);
-
